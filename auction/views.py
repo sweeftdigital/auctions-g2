4907800af10b8e8cf387fd47508598277663c1e8
sandronadiradze <sandro.nadiradze@makingscience.com>
2024-09-30 21:15:07 +0400

    chore: apply filterset to auction list view

--- auction/views.py
+++ auction/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from auction.filters import AuctionFilterSet
 from auction.models import Auction
 from auction.serializers import AuctionSerializer
 
@@ -9,3 +10,4 @@ class AuctionListView(ListAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Auction.objects.all()
     serializer_class = AuctionSerializer
+    filterset_class = AuctionFilterSet
