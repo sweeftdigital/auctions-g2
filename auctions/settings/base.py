a21c7c8fac07ab3164a18315099c7bbfafc552ef
sandronadiradze <sandro.nadiradze@makingscience.com>
2024-10-02 18:25:44 +0400

    feat: add custom page paginator class

--- auctions/settings/base.py
+++ auctions/settings/base.py
@@ -113,6 +113,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "auction.pagination.CustomPageNumberPagination",
 }
 
 SPECTACULAR_SETTINGS = {
