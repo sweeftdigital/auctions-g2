e1342cd0c81acf4142326e6f5fce227f56e8e60d
sandronadiradze <sandro.nadiradze@makingscience.com>
2024-10-03 18:51:28 +0400

    chore: modify target class in custom jwt auth scheme class to point to custom jwt auth class

--- auction/authentication/jwt_auth_scheme.py
+++ auction/authentication/jwt_auth_scheme.py
@@ -2,7 +2,7 @@ from drf_spectacular.extensions import OpenApiAuthenticationExtension
 
 
 class CustomJWTAuthenticationScheme(OpenApiAuthenticationExtension):
-    target_class = "auction.authentication.jwt_auth_scheme.CustomJWTAuthenticationScheme"
+    target_class = "auction.authentication.custom_jwt_auth.CustomJWTAuthentication"
     name = "BearerAuth"
 
     def get_security_definition(self, auto_schema):
