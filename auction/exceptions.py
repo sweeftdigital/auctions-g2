33fa1433fec27918cf6d03b406929e5c6e0bdcc0
sandronadiradze <sandro.nadiradze@makingscience.com>
2024-10-07 15:55:52 +0400

    chore: remove redundant if statement in exception formatter for 412_PRECONDITION_FAILED responses

--- auction/exceptions.py
+++ auction/exceptions.py
@@ -4,22 +4,6 @@ from drf_standardized_errors.types import ErrorResponse
 
 class CustomExceptionFormatter(ExceptionFormatter):
     def format_error_response(self, error_response: ErrorResponse):
-        if error_response is not None:
-            if error_response.errors[0].code in ["precondition_failed"]:
-                response = {
-                    "type": error_response.type,
-                    "errors": [
-                        {
-                            **{
-                                error.attr: error.detail
-                                for error in error_response.errors
-                            },
-                            "field_name": None,
-                        }
-                    ],
-                }
-                return response
-
         return {
             "type": error_response.type,
             "errors": [
