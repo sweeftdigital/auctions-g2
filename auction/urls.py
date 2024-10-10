7a882d41865ab9155b7295090ef6310ab870ee78
sandronadiradze <sandro.nadiradze@makingscience.com>
2024-10-10 16:36:56 +0400

    chore: modify naming in urls for auctions and bookmarks

--- auction/urls.py
+++ auction/urls.py
@@ -14,35 +14,35 @@ from auction.views import (
 
 urlpatterns = [
     path(
-        "buyer/auctions/list/",
+        "buyer/list/",
         BuyerAuctionListView.as_view(),
         name="auction-list-buyer",
     ),
     path(
-        "seller/auctions/list/",
+        "seller/list/",
         SellerAuctionListView.as_view(),
         name="auction-list-seller",
     ),
     path(
-        "auction/retrieve/<uuid:id>/",
+        "retrieve/<uuid:id>/",
         RetrieveAuctionView.as_view(),
         name="retrieve-auction",
     ),
     path(
-        "auction/create/live/",
+        "create/auction/",
         CreateLiveAuctionView.as_view(),
         name="create-live-auction",
     ),
     path(
-        "auction/create/draft/",
+        "create/draft/",
         CreateDraftAuctionView.as_view(),
         name="create-draft-auction",
     ),
-    path("auction/delete/<uuid:id>/", DeleteAuctionView.as_view(), name="delete-auction"),
+    path("delete/<uuid:id>/", DeleteAuctionView.as_view(), name="delete-auction"),
     path("bookmarks/list/", BookmarkListView.as_view(), name="list-bookmark"),
-    path("bookmarks/create/", CreateBookmarkView.as_view(), name="create-bookmark"),
+    path("bookmark/create/", CreateBookmarkView.as_view(), name="create-bookmark"),
     path(
-        "bookmarks/delete/<uuid:pk>/",
+        "bookmark/delete/<uuid:pk>/",
         DeleteBookmarkView.as_view(),
         name="delete-bookmark",
     ),
