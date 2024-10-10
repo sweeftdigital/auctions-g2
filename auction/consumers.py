f77c69ac26ce9b7fcf666e1147cf028550b28a5a
sandronadiradze <sandro.nadiradze@makingscience.com>
2024-10-10 16:22:31 +0400

    chore: add method for getting language used in translations from headers

--- auction/consumers.py
+++ auction/consumers.py
@@ -1,19 +1,59 @@
+import json
 from abc import ABC, abstractmethod
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from django.utils.translation import activate
 from django.utils.translation import gettext_lazy as _
 
 
-class BaseAuctionConsumer(AsyncJsonWebsocketConsumer, ABC):
+class CustomAsyncJsonWebsocketConsumer(AsyncJsonWebsocketConsumer):
+    async def send_json(self, content, close=False, ensure_ascii=False):
+        """
+        Encode the given content as JSON and send it to the client,
+        with optional ensure_ascii parameter (default is False).
+        """
+        # Override encode_json to include the ensure_ascii parameter
+        await super().send(
+            text_data=self.encode_json(content, ensure_ascii=ensure_ascii), close=close
+        )
+
+    @classmethod
+    def encode_json(cls, content, ensure_ascii=False):
+        return json.dumps(content, ensure_ascii=ensure_ascii)
+
+
+class BaseAuctionConsumer(CustomAsyncJsonWebsocketConsumer, ABC):
     async def connect(self):
         user = self.scope["user"]
         if user.is_anonymous:
             await self.close()
             return
 
+        # Activate language, this is used for translating responses.
+        self.language = self.get_language_from_headers()
+        activate(self.language)
+
         await self.accept()
         await self.add_to_group()
         await self.on_connect_success()
+
+    def get_language_from_headers(self):
+        """
+        Extracts the language from the Accept-Language header.
+
+        Returns:
+            str: The language code if found, otherwise None.
+        """
+        headers = dict(self.scope["headers"])
+        accept_language = headers.get(b"accept-language")
+        if accept_language:
+            # Decode and split languages by quality score, choosing the highest-ranked one
+            languages = accept_language.decode().split(",")
+            primary_language = languages[0].split(";")[
+                0
+            ]  # Take the primary language code
+            return primary_language
+        return None
 
     @abstractmethod
     async def add_to_group(self):
@@ -31,6 +71,7 @@ class BaseAuctionConsumer(AsyncJsonWebsocketConsumer, ABC):
             event (dict): The event data.
             additional_data (dict, optional): Additional data to include in the notification.
         """
+        activate(self.language)
         data = event.get("data")
         destination = self.get_destination()
         message = str(_("New auction was successfully created."))
@@ -44,7 +85,7 @@ class BaseAuctionConsumer(AsyncJsonWebsocketConsumer, ABC):
                 "message": message,
                 "destination": destination,
                 "data": data,
-            }
+            },
         )
 
     @abstractmethod
@@ -100,6 +141,7 @@ class SellerAuctionConsumer(BaseAuctionConsumer):
         await self.channel_layer.group_discard("auctions_for_bidders", self.channel_name)
 
     async def reset_user_counter(self):
+        activate(self.language)
         self.new_auction_count = 0
         data = {"new_auction_count": self.new_auction_count}
         await self.send_json(
--- auction/routing.py
+++ auction/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .consumers import BuyerAuctionConsumer, SellerAuctionConsumer
 
 websocket_urlpatterns = [
-    path("ws/auctions/seller/dashboard/", SellerAuctionConsumer.as_asgi()),
-    path("ws/auctions/buyer/dashboard/", BuyerAuctionConsumer.as_asgi()),
+    path("ws/seller/dashboard/", SellerAuctionConsumer.as_asgi()),
+    path("ws/buyer/dashboard/", BuyerAuctionConsumer.as_asgi()),
 ]
