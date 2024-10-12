8c4592a40717cb013a996a0e43e265c6c65558c2
sandronadiradze <sandro.nadiradze@makingscience.com>
2024-10-12 16:37:25 +0400

    chore: modify page_size variable in custom pagination class, add max_page_size variable and add page_size query param for specifiying exact amount of items per page

--- auction/pagination.py
+++ auction/pagination.py
@@ -2,5 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 50
+    page_size = 10
     last_page_strings = ("last",)
+    page_size_query_param = "page_size"
+    max_page_size = 100
