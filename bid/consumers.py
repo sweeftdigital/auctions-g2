cc14a75c024bd5a76001becf1fb9b2d72981229b
Nika Alaverdashvili <nikoloz.alaverdashvili.1@btu.edu.ge>
2024-10-13 18:31:13 +0400

    feat: handle real-time WebSocket notifications for updated bids

--- bid/consumers.py
+++ bid/consumers.py
@@ -7,13 +7,9 @@ class BidConsumer(AsyncJsonWebsocketConsumer):
         self.auction_id = self.scope["url_route"]["kwargs"]["auction_id"]
 
         if not user.is_authenticated or not user.is_seller:
-            print("*" * 2000)
             await self.close()
             return
 
-        print(self.auction_id, "*" * 200)
-
-        # Add the user to the auction-specific group
         await self.channel_layer.group_add(
             f"auction_{self.auction_id}", self.channel_name
         )
@@ -22,7 +18,6 @@ class BidConsumer(AsyncJsonWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, code):
-        """self.auction_id is available before discarding the group"""
         await self.channel_layer.group_discard(
             f"auction_{self.auction_id}", self.channel_name
         )
@@ -31,13 +26,21 @@ class BidConsumer(AsyncJsonWebsocketConsumer):
     async def new_bid_notification(self, event):
         """
         Handles real-time notifications of new bids.
-        The `event` contains the full bid data.
         """
         await self.send_json(
             {
                 "type": "new_bid_notification",
-                "bid": event[
-                    "message"
-                ],  # Send the full bid data in the WebSocket message
+                "bid": event["message"],
+            }
+        )
+
+    async def updated_bid_notification(self, event):
+        """
+        Handles real-time notifications of updated bids.
+        """
+        await self.send_json(
+            {
+                "type": "updated_bid_notification",
+                "bid": event["message"],
             }
         )
