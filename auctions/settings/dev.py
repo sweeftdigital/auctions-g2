ed0df3f42bc3194b0b3b562fcec8c1185ced2f55
sandronadiradze <sandro.nadiradze@makingscience.com>
2024-10-29 11:43:09 +0400

    feat: add logging to the project

--- auctions/settings/dev.py
+++ auctions/settings/dev.py
@@ -3,3 +3,35 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s | %(asctime)s | %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "file": {  # DEBUG level logs will be saved in files
+            "level": "DEBUG",
+            "class": "logging.handlers.RotatingFileHandler",
+            "maxBytes": 1024 * 1024 * 10,  # 10 MB
+            "filename": BASE_DIR / "logs/app/app.log",
+            "formatter": "verbose",
+            "backupCount": 5,
+        },
+        "console": {  # Output INFO level logs to console
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file", "console"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
